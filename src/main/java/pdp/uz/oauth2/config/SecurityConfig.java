e6b7fc6bae8453dba4342cb63960ca8568b47aed
Samir <sharipovsamir480@gmail.com>
2024-08-27 17:50:04 +0500

    security

--- src/main/java/pdp/uz/oauth2/config/SecurityConfig.java
+++ src/main/java/pdp/uz/oauth2/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import pdp.uz.oauth2.service.CustomOidcUserService;
 
@@ -11,33 +13,23 @@ import pdp.uz.oauth2.service.CustomOidcUserService;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//       return http
-//               .authorizeHttpRequests( auth -> {
-//                   auth.requestMatchers("/").permitAll();
-//                   auth.anyRequest().authenticated();
-//               })
-//               .oauth2Login(withDefaults())
-//               .build();
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOidcUserService customOidcUserService) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF disable qilish
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //session ni o'chirish
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/").permitAll()
-                                .anyRequest().authenticated()
+                                .requestMatchers("/").permitAll() // ochiq yollar
+                                .anyRequest().authenticated() //qolgan barcha yollar authentifikatsiyadan otish soraladi
                 )
                 .oauth2Login(oauth2Login ->
                         oauth2Login
-                                .loginPage("/oauth2/authorization/google")
+                                .loginPage("/oauth2/authorization/google") //authentication uchun oauth2 yolini belgilash
                                 .userInfoEndpoint(userInfoEndpoint ->
                                         userInfoEndpoint.oidcUserService(customOidcUserService)
                                 )
                 );
-
         return http.build();
     }
 }
